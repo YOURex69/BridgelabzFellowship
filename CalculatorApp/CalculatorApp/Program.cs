ab4bc254653984666d525605f4ba85c76b6a57d9
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-10-30 17:19:21 +0530

    Add Calculator and Helloworld program

---
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            calculatorMethods obj = new calculatorMethods();
+            Console.WriteLine("Want to use calculator...y/n");
+            char ch1 = Convert.ToChar(Console.ReadLine());
+            char consent = ch1;
+            
+            while(consent=='y'||consent=='Y')
+            {
+                Console.WriteLine("Enter options from 1-4: ");
+                int key = Convert.ToInt32(Console.ReadLine());
+                switch(key)
+                { 
+                    case 1:
+                    Console.WriteLine("enter 2 numbers:");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    int sum = obj.getsum(num1, num2);
+                    Console.WriteLine("Sum is" + sum);
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Console.WriteLine("enter 2 numbers:");
+                    int num3 = Convert.ToInt32(Console.ReadLine());
+                    int num4 = Convert.ToInt32(Console.ReadLine());
+                    int dif = obj.getdif(num3, num4);
+                    Console.WriteLine("Difference is" + dif);
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    Console.WriteLine("enter 2 numbers:");
+                    int num5 = Convert.ToInt32(Console.ReadLine());
+                    int num6 = Convert.ToInt32(Console.ReadLine());
+                    int mul = obj.getmul(num5, num6);
+                    Console.WriteLine("Multiplication is" + mul);
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    Console.WriteLine("enter 2 numbers:");
+                    int num7 = Convert.ToInt32(Console.ReadLine());
+                    int num8 = Convert.ToInt32(Console.ReadLine());
+                    int div = obj.getsum(num7, num8);
+                    Console.WriteLine("Division is" + div);
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Please Enter Input between 1-4");
+                    break;
+                }
+                Console.WriteLine("Continue...y/n");
+                char ch = Convert.ToChar(Console.ReadLine());
+                consent = ch;
+            }
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorApp/calculatorMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorApp
+{
+    
+    public class calculatorMethods
+    {
+        int total;
+        public int getsum(int num1,int num2)
+        {
+            return total = num1 + num2;
+        }
+        public int getdif(int num1,int num2)
+        {
+            return total = num1 - num2;
+        }
+        public int getmul(int num1,int num2)
+        {
+            return total = num1 * num2;
+        }
+        public int getdiv(int num1,int num2)
+        {
+            return total = num1 / num2;
+        }
+    }
+}
