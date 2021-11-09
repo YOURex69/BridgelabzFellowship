e8a128e46e43c4b2bbf9aa598d2f489bb4905ba4
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-09 18:14:04 +0530

    Add Employee wage uc9

---
+++ EmployeeWageAndSaveTotalWage/EmployeeWageAndSaveTotalWage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageAndSaveTotalWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("dMart", 20, 2, 10);
+            dMart.computeEmpWage();
+        }
+    }
+}
