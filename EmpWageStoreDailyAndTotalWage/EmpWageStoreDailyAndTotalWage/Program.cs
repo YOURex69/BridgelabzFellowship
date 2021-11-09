6bfe47c15082476301a75bf15bbfe2241764bc91
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-09 19:15:53 +0530

    Add program to store daily and total wage

---
+++ EmpWageStoreDailyAndTotalWage/EmpWageStoreDailyAndTotalWage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmpWageStoreDailyAndTotalWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWageBuilder dMart = new EmpWageBuilder("dMart", 20, 20, 200);
+            dMart.computeEmpWage();
+        }
+    }
+}
