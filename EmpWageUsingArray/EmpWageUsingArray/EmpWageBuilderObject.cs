e89383e1a3560819eea9015150ae2239e13be16a
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-12-02 14:28:34 +0530

    Added

---
+++ EmpWageUsingArray/EmpWageUsingArray/EmpWageBuilderObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageUsingArray
+{
+    public class EmpWageBuilderObject
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string company = "";
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+
+        }
+        public int computeEmpWage()
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs < this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random rand = new Random();
+                int empCheck = rand.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
+
+            }
+
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            //  Console.WriteLine("Total Emp Wage for :" + company + "is :" + totalEmpWage);
+            return totalEmpWage;
+        }
+    }
+}
---
+++ EmpWageUsingArray/EmpWageUsingArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmpWageUsingArray
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] Arr = new int[5];
+            int i = 0;
+           EmpWageBuilderObject dMart = new EmpWageBuilderObject("dMart", 20, 2, 10);
+            EmpWageBuilderObject google = new EmpWageBuilderObject("Google", 20, 2, 10);
+            Arr[i]=dMart.computeEmpWage();
+            i++;
+            Arr[i] = google.computeEmpWage();
+            for(int j=0;j<2;j++)
+            {
+                Console.WriteLine(" Emp wage is " + Arr[j]);
+            }
+        }
+    }
+}
